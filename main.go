ead35d2153bb10974b53f59794cd5eccebf7e10e
Samuel Cox <samc1253@gmail.com>
2023-11-20 16:02:17 -0700

    Adjust go.main to run a command line interface

--- main.go
+++ main.go
@@ -7,11 +7,28 @@ import (
 )
 
 func main() {
-	myCreditCard := creditCard.NewCreditCard("6011797668868728", 1000)
+	fmt.Println("Welcome to Credit Card Validator!")
+
+	fmt.Print("Enter a credit card number: ")
+	var number string
+	fmt.Scanln(&number)
+
+	fmt.Print("Enter a credit card limit: ")
+	var limit int
+	fmt.Scanln(&limit)
+
+	fmt.Println("Initializing your credit card...")
+
+	myCreditCard := creditCard.NewCreditCard(number, limit)
 	b := bank.NewBank("My Bank")
-	if b.ValidTransaction(100, myCreditCard) {
-		fmt.Printf("The number %s is valid!", myCreditCard.GetNumber())
+
+	fmt.Print("Enter a transaction amount: ")
+	var amount int
+	fmt.Scanln(&amount)
+
+	if b.ValidTransaction(amount, myCreditCard) {
+		fmt.Printf("The transaction is valid!")
 	} else {
-		fmt.Printf("The number %s is invalid!", myCreditCard.GetNumber())
+		fmt.Printf("The transaction is invalid!")
 	}
 }
